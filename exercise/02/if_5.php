a8c1885b1808e8ed7ff587e7dcfb5bf03f75d6c6
YukiYouda <youda0518@yahoo.co.jp>
2021-09-13 14:36:45 +0900

    配列と連想配列の課題完了

---
+++ exercise/02/if_5.php
@@ -0,0 +1,18 @@
+<?php
+
+$num = 2;
+$count = 0;
+
+for ($i = 1; $i <= $num; $i++) {
+    if ($num % $i === 0) {
+        $count += 1;
+    }
+}
+
+if ($count === 2) {
+    echo "{$num}は素数です。";
+} else {
+    echo "{$num}は素数ではありません。";
+}
+
+
