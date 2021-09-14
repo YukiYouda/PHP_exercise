3173af939b4f12d8be09e84500c80fdb5a2b00f4
YukiYouda <youda0518@yahoo.co.jp>
2021-09-14 12:47:18 +0900

    if_5.php修正

--- exercise/02/if_5.php
+++ exercise/02/if_5.php
@@ -1,15 +1,18 @@
 <?php
 
-$num = 2;
+$num = 10000;
 $count = 0;
 
-for ($i = 1; $i <= $num; $i++) {
+for ($i = 2; $i <= $num; $i++) {
     if ($num % $i === 0) {
         $count += 1;
+        if ($count === 2) {
+            break;
+        }
     }
 }
 
-if ($count === 2) {
+if ($count === 1) {
     echo "{$num}は素数です。";
 } else {
     echo "{$num}は素数ではありません。";
